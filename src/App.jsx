2b06ff433ee64f653b5bba08ec1d7f198ea0479d
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-24 10:28:43 +0600

    add header or navbar setion layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import Header from "./pages/Header"
-
+import { FaUser } from "react-icons/fa";
 function App() {
 
   return (
-    <div className="bg-gradient-to-r from-[#f4dbf9] to-[#e0dafc] w-full h-screen">
-      <div className="r">
+    <div className="container mx-auto">
+      {/* navbar section or header layout */}
+      <div className="flex items-center justify-between py-3">
         <div>
-          <h1></h1>
+          <h1 className="text-2xl font-bold">Task Board</h1>
         </div>
         <div>
-           
+           <FaUser  className="w-16 h-16 rounded-full border bg-white p-3 cursor-pointer" />
         </div>
       </div>
-      <h1 className="">
         <Header />
-      </h1>
     </div>
   )
 }
