665894b620841ae7bb49175f08b86ee504edfc63
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-24 18:30:04 +0600

    add set taskData from database

--- src/pages/CreateTask.jsx
+++ src/pages/CreateTask.jsx
@@ -1,11 +1,36 @@
+/* eslint-disable react/prop-types */
+import axios from "axios";
 import { useForm } from "react-hook-form"
-const CreateTask = () => {
-    const { register, handleSubmit, reset} = useForm();
+import toast from "react-hot-toast";
+const CreateTask = ({ setIsOpen }) => {
+    const { register, handleSubmit, reset } = useForm();
 
-    const handleCreateTask = (data) =>{
-        console.log(data);
+    const handleCreateTask = async (data) => {
+        const taskData = {
+            assignees: data.assignees,
+            description: data.description,
+            priority: data.priority,
+            status: data.status,
+            team: data.team,
+            title: data.title,
+            user: 'sushil',
+            email: 'sushil@gmail.com',
+        }
+        try {
+            await axios.post('http://localhost:5000/task', taskData)
+                .then(res => {
+                    if (res.data?.insertedId) {
+                        setIsOpen(false)
+                        toast.success('create successfully!')
+                    }
+                })
+
+        }
+        catch (error) {
+            console.log(error);
+        }
     }
-    const handleReset = ()=>{
+    const handleReset = () => {
         return reset();
     }
     return (
@@ -14,7 +39,7 @@ const CreateTask = () => {
             <form onSubmit={handleSubmit(handleCreateTask)} className="space-y-5">
                 <div className="flex items-center justify-between">
                     <label className="w-1/3 text-base font-bold">Title:</label>
-                    <input type="text" name="title" className="border w-full bg-[#c5c0c0]" {...register("title", { required: true })} required />
+                    <input type="text" name="title" className="border w-full bg-[#c5c0c0] p-2" {...register("title", { required: true })} required />
                 </div>
                 <div className="flex items-center justify-between">
                     <label className="w-1/3 text-base font-bold">Description:</label>
@@ -22,16 +47,16 @@ const CreateTask = () => {
                 </div>
                 <div className="flex items-center justify-between">
                     <label className="w-1/3 text-base font-bold">Team:</label>
-                    <input type="text" name="team" className="border w-full bg-[#c5c0c0]"  {...register("team", { required: true })} required />
+                    <input type="text" name="team" className="border w-full bg-[#c5c0c0] p-2"  {...register("team", { required: true })} required />
                 </div>
                 <div className="flex items-center justify-between">
                     <label className="w-1/3 text-base font-bold">Assignees:</label>
-                    <input type="text" name="assignees" className="border w-full bg-[#c5c0c0]"  {...register("assignees", { required: true })} required />
+                    <input type="text" name="assignees" className="border w-full bg-[#c5c0c0] p-2"  {...register("assignees", { required: true })} required />
                 </div>
                 <div className="flex items-center justify-between flex-col md:flex-row lg:flex-row gap-3">
                     <div className="flex items-center justify-start gap-5">
                         <h1 className="text-base font-bold ">Sort By:  </h1>
-                        <select name="priority" id="sortBy" className="px-2 py-1 rounded bg-[#c5c0c0]" {...register("priority", { required: true })} required>
+                        <select name="priority" id="sortBy" className="p-2 py-1 rounded bg-[#c5c0c0]" {...register("priority", { required: true })} required>
                             <option className="divide-y" value="p1">P1</option>
                             <option value="p0">P0</option>
                             <option value="p1">P1</option>
@@ -40,7 +65,7 @@ const CreateTask = () => {
                     </div>
                     <div className="flex items-center justify-start gap-5">
                         <h1 className="text-base font-bold ">Status:  </h1>
-                        <select name="status"  className="px-2 py-1 rounded bg-[#c5c0c0] text-black" {...register("status", { required: true })} required>
+                        <select name="status" className="px-2 py-1 rounded bg-[#c5c0c0] text-black p-2" {...register("status", { required: true })} required>
                             <option value="p1" selected>Pending</option>
                             <option value="p0">in Progress</option>
                             <option value="p1">Completed</option>
@@ -49,8 +74,9 @@ const CreateTask = () => {
                         </select>
                     </div>
                 </div>
+
                 <div className="flex items-center justify-end gap-5 mt-5">
-                    <button onClick={handleReset} className="bg-[#26689a] text-white px-5 py-1 rounded">Reset</button>
+                    <button type="btn" onClick={handleReset} className="bg-[#26689a] text-white px-5 py-1 rounded absolute right-[7.75rem] bottm-0">Reset</button>
                     <button type="submit" className="bg-[#26689a] text-white px-5 py-1 rounded">Submit</button>
                 </div>
             </form>
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
                 </select>
             </div>
             <Modal isOpen={isOpen} setIsOpen={setIsOpen} >
-                <CreateTask />
+                <CreateTask setIsOpen={setIsOpen} />
             </Modal>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 text-white text-[18px]">
                 <div className="bg-white">
