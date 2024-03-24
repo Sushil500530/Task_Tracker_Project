5d0ee654127c4cd59cbf52a10060a719d0077bfe
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-24 11:10:10 +0600

    add modal when the create button click

--- src/components/shared/Modal.jsx
+++ src/components/shared/Modal.jsx
@@ -1,9 +1,37 @@
+/* eslint-disable react/prop-types */
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
+import { RxCross2 } from "react-icons/rx";
 
-const Modal = () => {
+
+const Modal = ({ isOpen, setIsOpen, children }) => {
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+
     return (
-        <div>
-            
-        </div>
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={()=>setIsOpen(true)}>
+                <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0" >
+                    <div className="fixed inset-0 bg-black/25" />
+                </Transition.Child>
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0 scale-95" enterTo="opacity-100 scale-100" leave="ease-in duration-200" leaveFrom="opacity-100 scale-100" leaveTo="opacity-0 scale-95" >
+                            <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                                {children}
+                                <div className="absolute  top-0 right-0">
+                                    <button type="button" onClick={closeModal} >
+                                        <RxCross2 className='w-10 h-10 bg-gray-200 hover:text-red-500 rounded-full p-2 text-2xl' />
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
     );
 };
 
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import Modal from "../components/shared/Modal";
 
 const Header = () => {
+    let [isOpen, setIsOpen] = useState(true);
+
+    function openModal() {
+        setIsOpen(true)
+    }
     return (
         <div className="space-y-5">
+            {/* filter by name, priority, date  */}
             <div className="flex items-center justify-between flex-col md:flex-row lg:flex-row gap-5">
                 <div className="flex items-center flex-col md:flex-row lg:flex-row justify-start gap-2 lg:gap-5">
                     <h1 className="text-xl font-bold ">Filter By: </h1>
@@ -12,10 +20,11 @@ const Header = () => {
                         <option value="p1">P1</option>
                         <option value="p2">P2</option>
                     </select>
-                    <input type="date" name="date" id="" className="px-2 py-1 rounded"  />
+                    <input type="date" name="date" id="" className="px-2 py-1 rounded" />
                 </div>
-                <button className="px-10 py-1 bg-[#26689a] text-white rounded-md">Add New Task</button>
+                <button  onClick={openModal} className="px-10 py-1 bg-[#26689a] text-white rounded-md">Add New Task</button>
             </div>
+            {/* sort by priority  */}
             <div className="flex items-center justify-center md:justify-start lg:justify-start gap-5">
                 <h1 className="text-xl font-bold ">Sort By:  </h1>
                 <select name="sortBy" id="sortBy" className="px-2 py-1 rounded" >
@@ -25,6 +34,9 @@ const Header = () => {
                     <option value="p2">P2</option>
                 </select>
             </div>
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} >
+                <h1>Create New Task</h1>
+            </Modal>
         </div>
     );
 };
