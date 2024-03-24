76a70764383911468ff754ff3cf30d90575fd5f4
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-24 10:53:41 +0600

    add task header for filter form

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { FaUser } from "react-icons/fa";
 function App() {
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto space-y-8">
       {/* navbar section or header layout */}
-      <div className="flex items-center justify-between py-3">
+      <div className="flex items-center justify-between p-5">
         <div>
           <h1 className="text-2xl font-bold">Task Board</h1>
         </div>
@@ -13,7 +13,9 @@ function App() {
            <FaUser  className="w-16 h-16 rounded-full border bg-white p-3 cursor-pointer" />
         </div>
       </div>
-        <Header />
+      <div className="w-full h-full p-5 border border-white rounded-lg">
+      <Header />
+      </div>
     </div>
   )
 }
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,8 +1,30 @@
 
 const Header = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold text-center">this is Header</h1>
+        <div className="space-y-5">
+            <div className="flex items-center justify-between flex-col md:flex-row lg:flex-row gap-5">
+                <div className="flex items-center flex-col md:flex-row lg:flex-row justify-start gap-2 lg:gap-5">
+                    <h1 className="text-xl font-bold ">Filter By: </h1>
+                    <input type="text" name="assignmentName" placeholder="Assignment Name" className="px-2 py-1 rounded w-full md:w-1/3 lg:w-1/3" />
+                    <select name="sortBy" id="sortBy" className="px-2 py-1 rounded" >
+                        <option disabled selected>Priority</option>
+                        <option value="p0">P0</option>
+                        <option value="p1">P1</option>
+                        <option value="p2">P2</option>
+                    </select>
+                    <input type="date" name="date" id="" className="px-2 py-1 rounded"  />
+                </div>
+                <button className="px-10 py-1 bg-[#26689a] text-white rounded-md">Add New Task</button>
+            </div>
+            <div className="flex items-center justify-center md:justify-start lg:justify-start gap-5">
+                <h1 className="text-xl font-bold ">Sort By:  </h1>
+                <select name="sortBy" id="sortBy" className="px-2 py-1 rounded" >
+                    <option disabled selected>Priority</option>
+                    <option value="p0">P0</option>
+                    <option value="p1">P1</option>
+                    <option value="p2">P2</option>
+                </select>
+            </div>
         </div>
     );
 };
