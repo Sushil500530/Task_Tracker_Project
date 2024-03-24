8f87def14d5beec06c22bd98d7a51ef734cb36ce
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-24 21:32:23 +0600

    add find current data and show updata data in modal

--- src/components/shared/TaskCard.jsx
+++ src/components/shared/TaskCard.jsx
@@ -1,29 +1,53 @@
+/* eslint-disable react/prop-types */
 import { HiDotsVertical } from "react-icons/hi";
+import Modal from "./Modal";
+import EditTask from "../../pages/EditTask";
+import { useState } from "react";
+import useTasks from "../hooks/useTasks";
 
-const TaskCard = () => {
+const TaskCard = ({currentTask}) => {
+    const [tasks,refetch] = useTasks();
+    let [isOpen, setIsOpen] = useState(false);
+    const [editTask,setEditTask] = useState([]);
 
+    function openModal() {
+        setIsOpen(true)
+    }
+    const handleEdit = (id) => {
+        const currentData = tasks.find(task => task._id === id);
+        setEditTask(currentData)
+        openModal();
+    }
+    // console.log(currentTask);
+    console.log(editTask);
     return (
-        <div className="text-black bg-gray-200 shadow-md p-2 m-2">
-            <div className="flex items-center justify-between">
-                <h1 className="text-xl font-bold">Taks1</h1>
-                <h1 className="bg-[#26689a] text-white p-1 rounded">P0</h1>
-            </div>
-            <hr className="text-black bg-gray-400 w-full h-[3px] my-3" />
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam debitis autem vero quidem in unde. </p>
-            <div className="flex items-center justify-between">
-                <h1 className="text-xl font-bold">@Praving</h1>
-                <div className="group relative">
-                <button className="bg-[#26689a] text-white p-1 rounded cursor-pointer"><HiDotsVertical /></button> 
-                    <div className=" hidden group-hover:block  bg-white absolute top-0 right-5 rounded w-28">
-                        <div className="flex flex-col items-center justify-center gap-1">
-                            <button className=" p-1 rounded cursor-pointer hover:bg-gray-200 w-full">Edit</button>
-                            <button className=" p-1 rounded cursor-pointer hover:bg-gray-200 w-full">Delete</button>
+        <>
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+                <EditTask editTask={editTask} />
+            </Modal>
+            <div className="text-black bg-gray-200 shadow-md p-2 m-2">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-xl font-bold capitalize">{currentTask?.title}</h1>
+                    <h1 className="bg-[#26689a] text-white p-1 rounded">{currentTask?.priority}</h1>
+                </div>
+                <hr className="text-black bg-gray-400 w-full h-[3px] my-3" />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam debitis autem vero quidem in unde. </p>
+                <div className="flex items-center justify-between">
+                    <h1 className="text-xl font-bold">@Praving</h1>
+                    <div className="group relative">
+                        <button className="bg-[#26689a] text-white p-1 rounded cursor-pointer"><HiDotsVertical /></button>
+                        <div className=" hidden group-hover:block  bg-white absolute top-0 right-5 rounded w-28">
+                            <div className="flex flex-col items-center justify-center gap-1">
+                                <button onClick={()=> handleEdit(currentTask?._id)} className=" p-1 rounded cursor-pointer hover:bg-gray-200 w-full">Edit</button>
+                                <button className=" p-1 rounded cursor-pointer hover:bg-gray-200 w-full">Delete</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div >
+                <h1 className="bg-[#26689a] text-white px-5 py-1 w-2/3 my-3 text-center rounded capitalize">{currentTask?.status}</h1>
             </div >
-            <h1 className="bg-[#26689a] text-white px-5 py-1 w-2/3 my-3 text-center rounded">Assign</h1>
-        </div >
+        </>
+
     );
 };
 
--- src/pages/EditTask.jsx
+++ src/pages/EditTask.jsx
@@ -1,30 +1,31 @@
+/* eslint-disable react/prop-types */
 
-const EditTask = () => {
+const EditTask = ({ editTask }) => {
     return (
         <div>
             <h1 className="text-xl font-bold uppercase mb-8">Edit Task</h1>
             <div className="space-y-5">
                 <div className="flex items-center justify-start flex-col">
                     <label className="w-full text-base font-bold">Title:</label>
-                    <input type="text" name="title" className="border w-full bg-[#c5c0c0]" />
+                    <input type="text" name="title" defaultValue={editTask?.title} className="border w-full bg-[#c5c0c0] cursor-not-allowed" />
                 </div>
                 <div className="flex items-center justify-start flex-col">
                     <label className="w-full text-base font-bold">Description:</label>
-                    <textarea name="description" id="" className="border w-full bg-[#c5c0c0]" cols="10" rows="3"></textarea>
+                    <textarea name="description" id="" defaultValue={editTask?.description} className="border w-full bg-[#c5c0c0] cursor-not-allowed" cols="10" rows="3"></textarea>
                 </div>
                 <div className="flex items-center justify-start flex-col">
                     <label className="w-full text-base font-bold">Team:</label>
-                    <input type="text" name="team" className="border w-full bg-[#c5c0c0]" />
+                    <input type="text" name="team" defaultValue={editTask?.team} className="border w-full bg-[#c5c0c0] cursor-not-allowed" />
                 </div>
                 <div className="flex items-center justify-start flex-col">
                     <label className="w-full text-base font-bold">Assignees:</label>
-                    <input type="text" name="title" className="border w-full bg-[#c5c0c0]" />
+                    <input type="text" name="assignees" defaultValue={editTask?.assignees} className="border w-full bg-[#c5c0c0] cursor-not-allowed" />
                 </div>
                 <div className="flex items-center justify-between flex-col md:flex-row lg:flex-auto gap-5 ">
                     <div className="flex items-center justify-center md:justify-start lg:justify-start gap-5">
                         <h1 className="text-base font-bold ">Sort By:  </h1>
-                        <select name="sortBy" id="sortBy" className="px-2 py-1 rounded bg-[#c5c0c0]" >
-                            <option className="divide-y" value="p1">P1</option>
+                        <select name="priority" id="priority" className="px-2 py-1 rounded bg-[#c5c0c0]" >
+                            <option className="divide-y" defaultValue={editTask?.priority} disabled value="p1">{editTask?.priority}</option>
                             <option value="p0">P0</option>
                             <option value="p1">P1</option>
                             <option value="p2">P2</option>
@@ -32,12 +33,13 @@ const EditTask = () => {
                     </div>
                     <div className="flex items-center justify-center md:justify-start lg:justify-start gap-5">
                         <h1 className="text-base font-bold ">Status:  </h1>
-                        <select name="sortBy" id="sortBy" className="px-2 py-1 rounded bg-[#c5c0c0]" >
-                            <option className="divide-y" value="p1">Completed</option>
-                            <option value="p1">Pending</option>
-                            <option value="p0">in Progress</option>
-                            <option value="p2">Deployed</option>
-                            <option value="p2">Deffered</option>
+                        <select name="status" id="status" className="px-2 py-1 rounded bg-[#c5c0c0]" >
+                            <option value="pending" defaultValue={editTask?.status} disabled selected>{editTask?.status}</option>
+                            <option value="pending">Pending</option>
+                            <option value="inProgress">in Progress</option>
+                            <option value="completed">Completed</option>
+                            <option value="deployed">Deployed</option>
+                            <option value="deffered">Deffered</option>
                         </select>
                     </div>
                 </div>
