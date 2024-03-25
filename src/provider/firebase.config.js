1c224d9e0aaf83b7964fad3cda4a0f71f41c7eb3
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-25 17:59:35 +0600

    add authentication element and other

---
+++ src/provider/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.envVITE_APIKEY,
+  authDomain: import.meta.envVITE_AUTHDOMAIN,
+  projectId: import.meta.envVITE_PROJECTID,
+  storageBucket: import.meta.envVITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.envVITE_MESSAGINGSENDERID,
+  appId: import.meta.envVITE_APPID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
