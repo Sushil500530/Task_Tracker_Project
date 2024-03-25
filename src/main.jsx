6525e7a6b50e3648b1ea8a0b741bf6bccb7ff601
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-25 18:26:45 +0600

    add authentication credientials and connection

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './router/router.jsx'
 import { Toaster } from 'react-hot-toast'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AuthProvider from './provider/AuthProvider.jsx'
 
 
 
@@ -12,10 +13,11 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <Toaster />
-    </QueryClientProvider>
-
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <Toaster />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
--- src/provider/firebase.config.js
+++ src/provider/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.envVITE_APIKEY,
-  authDomain: import.meta.envVITE_AUTHDOMAIN,
-  projectId: import.meta.envVITE_PROJECTID,
-  storageBucket: import.meta.envVITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.envVITE_MESSAGINGSENDERID,
-  appId: import.meta.envVITE_APPID
+  apiKey:  import.meta.env.VITE_APIKEY,
+  authDomain:  import.meta.env.VITE_AUTHDOMAIN,
+  projectId:  import.meta.env.VITE_PROJECTID,
+  storageBucket:  import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId:  import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
